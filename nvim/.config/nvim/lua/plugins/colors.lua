de32351cd20dceaf19fdcf7000b6c240b102e34c
trinverdale-bit <dud0007@dandenong-hs.vic.edu.au>
2025-02-23 11:26:41 +1100

    feat: change float colors

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -3,7 +3,7 @@ function ColorMyPencils(color)
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "@function.method.call", { fg = "#ebbcba", bold = false })
     vim.api.nvim_set_hl(0, "@module", { fg = "#31748f", bold = false })
 end
