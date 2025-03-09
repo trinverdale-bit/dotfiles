20d9eb6c507ae075219353358a7aa2ebef75427b
trinverdale-bit <dud0007@dandenong-hs.vic.edu.au>
2025-03-10 10:03:27 +1100

    feat: add back in custom colors

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -19,8 +19,8 @@ function ColorMyPencils(color)
         end,
     })
 
-    --vim.api.nvim_set_hl(0, "@function.method.call", { fg = "#ebbcba", bold = false })
-    --vim.api.nvim_set_hl(0, "@module", { fg = "#31748f", bold = false })
+    vim.api.nvim_set_hl(0, "@function.method.call", { fg = "#ebbcba", bold = false })
+    vim.api.nvim_set_hl(0, "@module", { fg = "#31748f", bold = false })
 end
 
 return {
