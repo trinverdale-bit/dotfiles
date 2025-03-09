94bfeb00b98efb3fc5179f9f83c934b100bf5712
trinverdale-bit <dud0007@dandenong-hs.vic.edu.au>
2025-03-09 17:41:20 +1100

    feat: some color changes

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,23 +1,27 @@
 function ColorMyPencils(color)
-    color = color or "rose-pine"
+    color = color or "rose-pine-moon"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+    --[[
     vim.api.nvim_create_autocmd("FileType", {
         pattern = "harpoon",
         callback = function()
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         end,
     })
+    --]]
 
+    --[[
     vim.api.nvim_create_autocmd("FileType", {
         pattern = "TelescopePrompt,TelescopeResults",
         callback = function()
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e2e" })
         end,
     })
+    --]]
 
     --vim.api.nvim_set_hl(0, "@function.method.call", { fg = "#ebbcba", bold = false })
     --vim.api.nvim_set_hl(0, "@module", { fg = "#31748f", bold = false })
