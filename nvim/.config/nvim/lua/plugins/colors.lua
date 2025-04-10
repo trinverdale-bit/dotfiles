7209196fb1ac7df266714e43881a9d79679f04fa
trinverdale-bit <dud0007@dandenong-hs.vic.edu.au>
2025-04-10 13:57:25 +1000

    feat: change some color settings in nvim

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-    color = color or "rose-pine"
+    color = color or "rose-pine-moon"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -19,8 +19,8 @@ function ColorMyPencils(color)
         end,
     })
 
-    vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#ffffff", bold = true })
-    vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#ffffff", bold = true })
+    --vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#ffffff", bold = true })
+    --vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#ffffff", bold = true })
     --vim.api.nvim_set_hl(0, "@function.method.call", { fg = "#ebbcba", bold = false })
     --vim.api.nvim_set_hl(0, "@module", { fg = "#31748f", bold = false })
 end
@@ -33,7 +33,7 @@ return {
             disable_background = true,
             styles = {
                 italic = false,
-                bold = false,
+                --bold = false,
             },
         })
 
